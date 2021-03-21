ab3196f041f21945aecff2beb40f0baf772ef60b
ruhulamin <mrashk197@gmail.com>
2021-03-22 00:16:16 +0600

    Edit form js added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+function show_edit_form(){
+    document.getElementById("profile_edit_form").classList.remove("d-none") 
+    document.getElementById("profile_edit_button").classList.add("d-none") 
+}
+function show_edit_button_container(){
+    document.getElementById("profile_edit_form").classList.add("d-none") 
+    document.getElementById("profile_edit_button").classList.remove("d-none") 
+} 
+
 $(document).ready(function(){
 
    $('.custom_menu li a.menu-item').click(function(){
