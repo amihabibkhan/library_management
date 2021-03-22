10cea1c9fcb389bef8a61a45ce845871a7542a47
Saidul islam <saidulislam49du@gmail.com>
2021-03-22 19:41:37 +0600

    Profile page updated

--- js/script.js
+++ js/script.js
@@ -1,17 +1,28 @@
 $(document).ready(function(){
 
    $('.custom_menu li a.menu-item').click(function(){
-        // $(this).addClass('active')
-        
+               
         if (!$(this).hasClass("active")) {
             $('.custom_menu li a.menu-item').removeClass('active')
             $(this).addClass('active')
         }
     })
     
-    // $('.custom_menu li a.menu-item').click(function(){
-    //     $(this).removeClass('active')
-    // })
+    $(window).scroll(function(){
+
+        var profileBanner = $('.profile-banner').outerHeight()
+        var topTobottom = $(window).scrollTop()
+
+        if(topTobottom > profileBanner){
+            $('.sidebar').addClass('stikey')
+
+        }else{
+            $('.sidebar').removeClass('stikey')
+        }
+
+
+
+    })
 
    
 });
